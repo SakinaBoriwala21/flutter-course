eb65d8686637ccb8dd774b17bfd097287ab0f3ba
SakinaBoriwala21 <sakina.boriwala21@gmail.com>
2019-03-06 19:16:10 +0530

    Add Side Drawer

---
+++ lib/pages/products_admin.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductsAdmin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: Center(child: Text('Product Manager'),),
+    );;
+  }
+}
