6cd9032430bb96c170274d8d83f0f1c0fbf38f6c
SakinaBoriwala21 <sakina.boriwala21@gmail.com>
2019-03-06 15:19:14 +0530

    Work with Lists and Conditionals

--- lib/products.dart
+++ lib/products.dart
@@ -3,21 +3,40 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products);
+  Products([this.products = const []]) {
+    print('Products Widget Constructor ');
+  }
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/healthy.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+     Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Center(
+        child: Text('No Products found, please add some!'),
+      );
+    }
+
+    return productCards;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/healthy.jpg'),
-                    Text(element)
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    print('Products Widget Constructor build() ');
+    return _buildProductList(); 
   }
 }
