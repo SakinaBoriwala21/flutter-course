2cd639d9f4cee72a549e4ed8ac7cb940c1568637
SakinaBoriwala21 <sakina.boriwala21@gmail.com>
2019-03-07 17:26:57 +0530

    handle user input
    - Render form to take Product Title, Description and Price as input
    - Render form for Login Page
    - Use different KeyBoard types for different fields like price input field loads a numbers only keyboard
    - Use Switch Widget
    - Store user input and use it

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Map<String, String>> _products = [];
+  List<Map<String, dynamic>> _products = [];
 
-  void _addProduct(Map<String, String> product) {
+  void _addProduct(Map<String, dynamic> product) {
     setState(() {
       _products.add(product);
     });
@@ -45,11 +45,10 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple,
       ),
-      // home: AuthPage(),
+      home: AuthPage(),
       routes: {
-        '/': (BuildContext context) =>
-            ProductsPage(_products, _addProduct, _deleteProduct),
-        '/admin': (BuildContext context) => ProductsAdminPage(),
+        '/products': (BuildContext context) => ProductsPage(_products),
+        '/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct),
       },
       onGenerateRoute: (RouteSettings settings) {
         final List<String> pathElements = settings.name.split('/');
@@ -68,7 +67,7 @@ class _MyAppState extends State<MyApp> {
       onUnknownRoute: (RouteSettings settings) {
         return MaterialPageRoute(
             builder: (BuildContext context) =>
-                ProductsPage(_products, _addProduct, _deleteProduct));
+                ProductsPage(_products));
       },
     );
   }
